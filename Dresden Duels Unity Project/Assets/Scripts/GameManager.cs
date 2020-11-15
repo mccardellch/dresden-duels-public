457398aa02bb7d436d9731fb8fc1f44ba6809af2
harrymac <hhm7826@rit.edu>
2020-11-14 19:13:48 -0500

    implemented title screen menu with all necessary resources and addressed any bugs

---
+++ Dresden Duels Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+	// no need yet
+	// probably store game loop here
+}
---
+++ Dresden Duels Unity Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private bool isPaused;
+    [SerializeField] private bool isMuted;
+
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivateMenu();
+        } else {
+            DeactivateMenu();
+        }
+    } 
+
+    public void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+
+
+    public void ToggleMusic(bool _isMuted)
+    {     
+        AudioListener.pause = _isMuted;
+    }
+
+     
+}
