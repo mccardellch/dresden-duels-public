18be2d1e69d793d92017a622168a9f6be75be482
Christian Eliopoulos <cge7497@rit.edu>
2020-11-19 15:41:41 -0500

    Changes name of PlayerShoot script to H_Attacks.
    Also adds in attack hitboxes for Harry's lightning up move.
    
    I figure I should push to avoid merge errors, these aren't very big changes.

--- Dresden Duels Unity Project/Assets/Scripts/H_Attacks.cs
+++ Dresden Duels Unity Project/Assets/Scripts/H_Attacks.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShoot : MonoBehaviour
+public class H_Attacks : MonoBehaviour
 {
 
     public GameObject pfFireball;
     public Transform spellFirePoint;
     public float fireballUseTimerMax;
     float fireballUseTimer;
-
 
     void Update()
     {
@@ -27,7 +26,8 @@ public class PlayerShoot : MonoBehaviour
 
     void Shoot()
     {
-        GameObject tempFireBall=Instantiate(pfFireball, spellFirePoint.position, spellFirePoint.rotation);
+        GameObject tempFireBall = Instantiate(pfFireball, spellFirePoint.position, spellFirePoint.rotation);
         Destroy(tempFireBall, .5f);
     }
+
 }
